e92462507bab7e75f95233277db8cd154f945b89
vokske <kevinmuigai2@gmail.com>
2024-06-10 15:50:57 +0300

    Pop opcode

--- pop.c
+++ pop.c
@@ -2,18 +2,14 @@
 
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp, *head;
-	(void) line_number;
+	stack_t *temp;
 
-	head = *stack;
-	if (head == NULL)
-		pop_error(line_number);
-	if (head->next == NULL)
-	{
-		temp = head;
-		free(temp);
-	}
-	temp = head->next->prev;
-	head = temp;
+	 if (*stack == NULL)
+		 pop_error(line_number);
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
 	free(temp);
 }
