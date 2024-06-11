b94c301e8b528ffdd631a0871e219ad35d055dbe
vokske <kevinmuigai2@gmail.com>
2024-06-11 11:57:05 +0300

    pchar opcode

--- pchar.c
+++ pchar.c
@@ -11,10 +11,10 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	char value; 
 	
-	value = (char)((*stack)->n);
 	if (*stack == NULL)
 		pchar_empty_stack(line_number);
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 		pchar_not_ascii(line_number);
-	printf("%c", value);
+	value = (char)((*stack)->n);
+	printf("%c\n", value);
 }
