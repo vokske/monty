bf3f9482ede912a4bdb22678dcd0dfd02f978b82
vokske <kevinmuigai2@gmail.com>
2024-06-11 14:08:17 +0300

    pstr opcode

--- pstr.c
+++ pstr.c
@@ -10,9 +10,7 @@
 void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *current = *stack;
-	char string[100];
-	char value;
-	int i = 0;
+
 	(void) line_number;
 
 	if (current == NULL)
@@ -20,12 +18,10 @@ void pstr(stack_t **stack, unsigned int line_number)
 
 	while (current != NULL)
 	{
-		if (current->n <= 0 || current->n > 127 )
+		if (current->n == 0 || !isascii(current->n))
 			break;
-		value = (char)(current->n);
-		string[i++] = value;
+		putchar(current->n);
 		current = current->next;
 	}
-	string[i] = '\0';
-	printf("%s\n", string);
+	putchar('\n');
 }
